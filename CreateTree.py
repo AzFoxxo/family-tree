23d20d3e0e6708dbb45aa597a03b14da84584212
Az Foxxo <knotgamestudio@gmail.com>
2024-11-25 03:14:13 +0000

    Changed family tree data

--- CreateTree.py
+++ CreateTree.py
@@ -12,50 +12,80 @@ def create_populated_family_tree() -> FamilyTree:
     family_tree: FamilyTree = FamilyTree()
 
     # Populate the family tree
-
-    #region First generation
-    # Adam and Jasmine
-    adam: int = family_tree.add_person(Person("Adam", "Wax", SimplifiedSex.MALE, datetime.date(1920, 5, 24)))
-    jasmine: int = family_tree.add_person(Person("Jasmine", "Wax", SimplifiedSex.FEMALE, datetime.date(1926, 4, 10)))
-    family_tree.set_partner(adam, jasmine)
     
-    # Stew and Amber
-    stew: Person = family_tree.add_person(Person("Stew", "Knotberry", SimplifiedSex.MALE, datetime.date(1930, 6, 6)))
-    eve: Person = family_tree.add_person(Person("Eve", "Knotberry", SimplifiedSex.FEMALE, datetime.date(1924, 6, 6)))
-    family_tree.set_partner(stew, eve)
+    #region Generation #1
+    adam: Person = Person("Adam", "Elderson-Copper", SimplifiedSex.MALE, datetime.date(1943, 3, 23))
+    lester: Person = Person("Lester", "Elderson-Copper", SimplifiedSex.MALE, datetime.date(1945, 5, 12))
+    family_tree.add_person(adam)
+    family_tree.add_person(lester)
+    family_tree.set_partner(adam, lester)
     
-    # Mark deceased people
-    adam.set_deceased(datetime.date(1991, 4, 12))
-    jasmine.set_deceased(datetime.date(1994, 11, 3))
-    eve.set_deceased(datetime.date(2003, 2, 20))
+    amber: Person = Person("Amber", "Copper", SimplifiedSex.FEMALE, datetime.date(1943, 3, 23))
+    family_tree.add_person(amber)
     
+    thomas: Person = Person("Thomas", "Emmersohn", SimplifiedSex.MALE, datetime.date(1943, 3, 23))
+    ginny: Person = Person("Ginny", "Emmersohn", SimplifiedSex.FEMALE, datetime.date(1944, 7, 8))
+    family_tree.add_person(thomas)
+    family_tree.add_person(ginny)
+    family_tree.set_partner(thomas, ginny)
+    
+    john: Person = Person("John", "Colder", SimplifiedSex.MALE, datetime.date(1930, 5, 2))
+    jeanette: Person = Person("Jeanette", "Colder", SimplifiedSex.FEMALE, datetime.date(1939, 6, 12))
+    family_tree.add_person(john)
+    family_tree.add_person(jeanette)
+    family_tree.set_partner(john, jeanette)
     #endregion
     
-    #region Second generation
-    # Amber and Stew's children
-    john: Person = family_tree.add_person(Person("John", "Knotberry", SimplifiedSex.MALE, datetime.date(1950, 3, 3), jasmine, adam))
-    mary: Person = family_tree.add_person(Person("Mary", "Knotberry", SimplifiedSex.FEMALE, datetime.date(1951, 12, 29), jasmine, adam))
-    lester: Person = family_tree.add_person(Person("Lester", "Knotberry", SimplifiedSex.MALE, datetime.date(1952, 2, 28), jasmine, adam))
+    #region Generation #2
+    greg: Person = Person("Greg", "Boulder", SimplifiedSex.MALE, datetime.date(1965, 3, 23))
+    carol: Person = Person("Carol", "Boulder",SimplifiedSex.FEMALE, datetime.date(1967, 5, 12))
+    family_tree.add_person(greg)
+    family_tree.add_person(carol)
+    family_tree.set_partner(greg, carol)
     
-    # Adam and Jasmine's children
-    jedward: Person = family_tree.add_person(Person("Jedward", "Wax", SimplifiedSex.MALE, datetime.date(1950, 5, 25), eve, stew))
-    jenna: Person = family_tree.add_person(Person("Jenna", "Wax", SimplifiedSex.MALE, datetime.date(1952, 4, 2), eve, stew))
+    bexton: Person = Person("Bexton", "Elderson-Copper", SimplifiedSex.MALE, datetime.date(1965, 3, 23), amber, lester)
+    family_tree.add_person(bexton)
     
-    # Amber and Stew's grandchildren
-    harry: Person = family_tree.add_person(Person("Harry", "Wax", SimplifiedSex.MALE, datetime.date(1948, 2, 28), eve, adam))
+    david: Person = Person("David", "Eyre", SimplifiedSex.MALE, datetime.date(1963, 4, 1))
+    sandra: Person = Person("Sandra", "Eyre", SimplifiedSex.FEMALE, datetime.date(1964, 8, 12))
+    family_tree.add_person(david)
+    family_tree.add_person(sandra)
+    family_tree.set_partner(david, sandra)
     
-    # Partner second generation
-    family_tree.set_partner(lester, jedward)
-    family_tree.set_partner(jenna, john)
+    jamie: Person = Person("Jamie", "Emmersohn", SimplifiedSex.MALE, datetime.date(1965, 3, 23), ginny, thomas)
+    dorothy: Person = Person("Dorothy", "Emmersohn", SimplifiedSex.FEMALE, datetime.date(1967, 5, 12), jeanette, john)
+    family_tree.add_person(jamie)
+    family_tree.add_person(dorothy)
+    family_tree.set_partner(jamie, dorothy)
     
+    frank: Person = Person("Frank", "Anderson", SimplifiedSex.MALE, datetime.date(1966, 8, 8))
+    jane: Person = Person("Jane", "Anderson", SimplifiedSex.FEMALE, datetime.date(1966, 8, 8))
+    family_tree.add_person(frank)
+    family_tree.add_person(jane)
+    family_tree.set_partner(frank, jane)
     #endregion
     
-    #region Third generation
-    # John and Jenna's children
-    jackson: Person = family_tree.add_person(Person("Jackson", "Knotberry-Wax", SimplifiedSex.MALE, datetime.date(1970, 5, 3), jenna, john))
-    Wirral: Person = family_tree.add_person(Person("Wirral", "Knotberry-Wax", SimplifiedSex.FEMALE, datetime.date(1973, 3, 17), jenna, john))
-    Paxton: Person = family_tree.add_person(Person("Paxton", "Knotberry-Wax", SimplifiedSex.MALE, datetime.date(1976, 5, 3), jenna, john))
+    #region Generation #3
+    clyde: Person = Person("Clyde", "Emmersohn", SimplifiedSex.MALE, datetime.date(1990, 3, 23), dorothy, jamie)
+    bethany: Person = Person("Bethany", "Anderson", SimplifiedSex.FEMALE, datetime.date(1990, 3, 23), jane, frank)
+    family_tree.add_person(clyde)
+    family_tree.add_person(bethany)
+    family_tree.set_partner(clyde, bethany)
     
+    james: Person = Person("James", "Eyre", SimplifiedSex.MALE, datetime.date(1990, 3, 23), sandra, david)
+    angie: Person = Person("Angie", "Eyre", SimplifiedSex.FEMALE, datetime.date(1992, 11, 12), carol, greg)
+    family_tree.add_person(james)
+    family_tree.add_person(angie)
+    family_tree.set_partner(james, angie)
+    
+    dylan: Person = Person("Dylan", "Boulder", SimplifiedSex.MALE, datetime.date(1990, 3, 23), carol, greg)
+    lee: Person = Person("Lee", "Elderson-Copper", SimplifiedSex.MALE, datetime.date(1990, 3, 23), carol, bexton)
+    family_tree.add_person(dylan)
+    family_tree.add_person(lee)
     #endregion
+    
+    #region Generation #4
+    family_tree.add_person(Person("Cornelia", "Emmersohn", SimplifiedSex.FEMALE, datetime.date(2015, 3, 23), angie, james))
+    family_tree.add_person(Person("Otto", "Emmersohn", SimplifiedSex.MALE, datetime.date(2010, 3, 23), bethany, clyde))
     
     return family_tree
